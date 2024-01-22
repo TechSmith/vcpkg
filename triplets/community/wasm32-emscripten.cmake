44255858de21054486ef124b400dd7bdd748f0e1
Tony Lambert <tonylambert@gmail.com>
2024-01-21 21:51:32 -0500

    Set '-pthread' for all emscripten builds

--- triplets/community/wasm32-emscripten.cmake
+++ triplets/community/wasm32-emscripten.cmake
@@ -17,8 +17,13 @@ if(NOT EXISTS "${EMSCRIPTEN_ROOT}/cmake/Modules/Platform/Emscripten.cmake")
    message(FATAL_ERROR "Emscripten.cmake toolchain file not found")
 endif()
 
+set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -pthread")
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -pthread")
+set(VCPKG_LINKER_FLAGS "${VCPKG_LINKER_FLAGS} -pthread")
+
 set(VCPKG_TARGET_ARCHITECTURE wasm32)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${EMSCRIPTEN_ROOT}/cmake/Modules/Platform/Emscripten.cmake")
+
